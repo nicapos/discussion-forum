035ad512bebb9de606834b2aa1f15dcc00151c89
fdoble12 <80149315+fdoble12@users.noreply.github.com>
2022-05-17 17:43:56 +0800

    adjusted login page

---
+++ js/login.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#login-btn").click(function(){
+        var username = $("#username").val();
+        var password = $("#password").val();
+        if(username == "fdoble" && password == "hello123")
+            $("#Register").submit();
+        else{
+            $("input").css({"border-color":"red"});
+        }
+    })
+});
