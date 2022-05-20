ea95527b365435e221d236279d09b4035f9c64e0
fdoble12 <80149315+fdoble12@users.noreply.github.com>
2022-05-20 21:52:30 +0800

    added js for login

--- js/login.js
+++ js/login.js
@@ -2,11 +2,31 @@ $(document).ready(function(){
     $("#login-btn").click(function(){
         var username = $("#username").val();
         var password = $("#password").val();
+
+        var home =""
+        var valid = 1;
+        
         if(username == "fdoble" && password == "hello123")
-            $("#Register").submit();
+            home = "home.html"; 
+        else if(username == "annika1" && password == "password2")
+            home = "home2.html"
+        else if(username == "julia2" && password == "bye12")
+            home = "home3.html";
+        else if(username == "charlie1" && password == "woof")
+            home = "home4.html";
+        else if(username == "admin" && password == "1234")
+            home = "home5.html";  
+
         else{
+            valid = 0;
             $("input").css({"border-color":"red"});
             $("#error").text("Username or Password is incorrect.");
+        }
+
+        if(valid == 1)
+        {
+            $("#login-form").attr('action',home)
+            $("#login-form").submit();
         }
     })
 });
