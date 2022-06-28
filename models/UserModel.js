2f67ac6a42c08aa84abbd5d6f413735ba5a5beb2
nicapos <annikaposadas22@gmail.com>
2022-06-28 14:22:10 +0800

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,6 +4,8 @@ var UserSchema = new mongoose.Schema({
     username: String,
     email: String,
     password: String,
+    aboutMe: String,
+    myInterests: String,
     subforums: [Number],
     threads: [Number]
 });
