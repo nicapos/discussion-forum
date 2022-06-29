38b19c272e8021696a46ed6c1b369c6d2e6cdd86
fdoble12 <80149315+fdoble12@users.noreply.github.com>
2022-06-29 10:57:11 +0800

    Update SubforumModel.js

--- models/SubforumModel.js
+++ models/SubforumModel.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 var SubforumSchema = new mongoose.Schema({
     // TODO create a Schema for Posts
-    subforumID: Number,
-    subfname: String,
+    subforumName: Number,   // ex. freedom-wall
+    title: String,          // ex. Freedom Wall
     threads: [Number],
     members: [String] //usernames
 });
