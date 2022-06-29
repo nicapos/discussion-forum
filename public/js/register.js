bbe4fe0a2496d122ca5faae74fe23231b88722aa
nicapos <annikaposadas22@gmail.com>
2022-06-29 22:26:20 +0800

    Add "confirm password" validation

--- public/js/register.js
+++ public/js/register.js
@@ -7,15 +7,29 @@ $(document).ready(function(){
             if(result.username == usernameInput)
             {
                 $('#errorReg').text("Username already taken");
-                $('#username').css("background-color", "red");
+                $('#username').css("border", "2px solid red");
                 $('#reg-button').attr ("disabled", true);
             }
             else
             {
-                $('#username').css("background-color", "#E3E3E3");
+                $('#username').css("border", "1px solid #E3E3E3");
                 $('#errorReg').text("");
                 $('#reg-button').attr ("disabled", false);
             }
         })
+    });
+
+    $('#cpw').keyup(function () {
+        if ( $('#password').val() != $('#cpw').val() ) {
+            $('#errorReg').text("Passwords do not match");
+            $('#password').css("border", "2px solid red");
+            $('#cpw').css("border", "2px solid red");
+            $('#reg-button').attr ("disabled", true);
+        } else {
+            $('#errorReg').text("");
+            $('#password').css("border", "1px solid #E3E3E3");
+            $('#cpw').css("border", "1px solid #E3E3E3");
+            $('#reg-button').attr ("disabled", false);
+        }
     });
 });
