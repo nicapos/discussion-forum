18aa055e2e3f17c485fb3d941e6938e63c0e42dc
nicapos <annikaposadas22@gmail.com>
2022-07-04 21:17:02 +0800

    Fix like/dislike bug
    Fixed bug where like/dislike count is incorrect when pressing like & dislike consecutively

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,94 +1,84 @@
 $(document).ready(function() {
 
-    /*
-        Store vote value as int from -1 to 1 where:
-         1  Liked thread
-         0  No vote
-        -1  Disliked thread
-    */
-    var vote = 0;
+    /* async vote updates */
+    const threadURL = $('.thread > a').attr('href');
+    const threadId = threadURL.split("/")[3];
 
-    // Get initial value of vote
-    if ( $('#like-btn').hasClass('active') )
-        vote = 1;
-    else if ( $('#dislike-btn').hasClass('active') )
-        vote = -1;
-
-
-    function toggleVoteBtns(vote) {
-        switch (vote) {
-            case 1: 
+    function addLike() {
+        $.post('/action/like', {threadId: threadId}, function (data, status) {
+            if (status) {
+                // update button
                 $('#like-btn').addClass('active');
-                $('#dislike-btn').removeClass('active');
-                break;
-            case -1:
-                $('#dislike-btn').addClass('active');
-                $('#like-btn').removeClass('active');
-                break;
-            default: // assumes vote == 0
-                $('#like-btn').removeClass('active');
-                $('#dislike-btn').removeClass('active');
-        }
+
+                // update vote count
+                let voteCount = parseInt( $('#votes').text() );
+                $('#votes').text(voteCount + 1);
+            }
+        });
     }
 
-    var threadURL = $('.thread > a').attr('href');
-    var threadId = threadURL.split("/")[3];
+    function removeLike() {
+        $.post('/action/removeLike', {threadId: threadId}, function (data, status) {
+            if (status) {
+                // update button
+                $('#like-btn').removeClass('active');
 
+                // update vote count
+                let voteCount = parseInt( $('#votes').text() );
+                $('#votes').text(voteCount - 1);
+            }
+        });
+    }
+
+    function addDislike() {
+        $.post('/action/dislike', {threadId: threadId}, function (data, status) {
+            if (status) {
+                // update button
+                $('#dislike-btn').addClass('active');
+
+                // update vote count
+                let voteCount = parseInt( $('#votes').text() );
+                $('#votes').text(voteCount - 1);
+            }
+        });
+    }
+
+    function removeDislike() {
+        $.post('/action/removeDislike', {threadId: threadId}, function (data, status) {
+            if (status) {
+                // update button
+                $('#dislike-btn').removeClass('active');
+
+                // update vote count
+                let voteCount = parseInt( $('#votes').text() );
+                $('#votes').text(voteCount + 1);
+            }
+        });
+    }
+
+    /* add btn onclicks */
     $('#like-btn').click(function() {
 
-        if ( $('#like-btn').hasClass('active') ) {
-            // Remove like vote
-            $.post('/action/removeLike', {threadId: threadId}, function (data, status) {
-                if (status) {
-                    toggleVoteBtns(0);
+        if ( $('#like-btn').hasClass('active') )
+            removeLike();
+        else if ( $('#dislike-btn').hasClass('active') ) {
+            removeDislike();
+            addLike();
+        } else
+            addLike();
 
-                    // update vote count
-                    let voteCount = parseInt( $('#votes').text() );
-                    $('#votes').text(voteCount - 1);
-                }
-            });
-        }
-        else {
-            // Add like vote
-            $.post('/action/like', {threadId: threadId}, function (data, status) {
-                if (status) {
-                    toggleVoteBtns(1);
-
-                    // update vote count
-                    let voteCount = parseInt( $('#votes').text() );
-                    $('#votes').text(voteCount + 1);
-                }
-            });
-        }
-        
     });
 
     $('#dislike-btn').click(function() {
-        if ( $('#dislike-btn').hasClass('active') ) {
-            // Remove dislike vote
-            $.post('/action/removeDislike', {threadId: threadId}, function (data, status) {
-                if (status) {
-                    toggleVoteBtns(0);
 
-                    // update vote count
-                    let voteCount = parseInt( $('#votes').text() );
-                    $('#votes').text(voteCount + 1);
-                }
-            });
-        }
-        else {
-            // Add dislike vote
-            $.post('/action/dislike', {threadId: threadId}, function (data, status) {
-                if (status) {
-                    toggleVoteBtns(-1);
-
-                    // update vote count
-                    let voteCount = parseInt( $('#votes').text() );
-                    $('#votes').text(voteCount - 1);
-                }
-            });
-        }
-    
+        if ( $('#dislike-btn').hasClass('active') )
+            removeDislike();
+        else if ( $('#like-btn').hasClass('active') ) {
+            removeLike();
+            addDislike();
+        } else
+            addDislike();
+            
     });
 
 });
